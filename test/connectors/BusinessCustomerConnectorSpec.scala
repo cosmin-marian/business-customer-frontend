5e10db5dbb010c589391bcba54da8eab1af6a770
Avinash Anand <avinash.b.anand@accenture.com>
2015-04-27 14:42:03 +0100

    mocked connectors in tests; 100% coverage;

--- test/connectors/BusinessCustomerConnectorSpec.scala
+++ test/connectors/BusinessCustomerConnectorSpec.scala
@@ -3,14 +3,40 @@ package connectors
 
 import java.util.UUID
 
+import config.BusinessCustomerFrontendAuditConnector
+import forms._
+import org.mockito.Matchers
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfter
+import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.Play
 import play.api.libs.json.{JsValue, Json}
 import play.api.test.Helpers._
 import uk.gov.hmrc.play.audit.http.HeaderCarrier
-import forms._
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.http.logging.SessionId
+import uk.gov.hmrc.play.http.ws._
+import uk.gov.hmrc.play.http.{HttpResponse, HttpGet, HttpPost}
 
-class BusinessCustomerConnectorSpec extends PlaySpec with OneServerPerSuite {
+import scala.concurrent.Future
+
+class BusinessCustomerConnectorSpec extends PlaySpec with OneServerPerSuite with MockitoSugar with BeforeAndAfter {
+
+  class MockHttp extends WSGet with WSPost {
+    override def auditConnector: AuditConnector = BusinessCustomerFrontendAuditConnector
+    override def appName = Play.configuration.getString("appName").getOrElse("business-customer-frontend")
+  }
+
+  val mockWSHttp = mock[MockHttp]
+
+  object TestBusinessCustomerConnector extends BusinessCustomerConnector {
+    override val http: HttpGet with HttpPost = mockWSHttp
+  }
+
+  before {
+    reset(mockWSHttp)
+  }
 
   "BusinessCustomerConnector" must {
 
@@ -24,7 +50,8 @@ class BusinessCustomerConnectorSpec extends PlaySpec with OneServerPerSuite {
 
         val businessDetails = BusinessDetails("UIB", SoleTraderMatch(None, None, None), LimitedCompanyMatch(None, None), UnincorporatedMatch(Some("ACME"), Some(1111111111)), OrdinaryBusinessPartnershipMatch(None, None), LimitedLiabilityPartnershipMatch(None, None))
         implicit val hc = new HeaderCarrier(sessionId = Some(SessionId(s"session-${UUID.randomUUID}")))
-        val result = BusinessCustomerConnector.lookup(businessDetails)
+        when(mockWSHttp.POST[BusinessDetails, HttpResponse](Matchers.any(), Matchers.any(), Matchers.any())(Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Future.successful(HttpResponse(200, Some(matchSuccessResponse))))
+        val result = TestBusinessCustomerConnector.lookup(businessDetails)
         await(result).as[JsValue] must be(matchSuccessResponse)
 
       }
@@ -32,7 +59,8 @@ class BusinessCustomerConnectorSpec extends PlaySpec with OneServerPerSuite {
       "for unsuccessful match, return error message" in {
         val businessDetails = BusinessDetails("UIB", SoleTraderMatch(None, None, None), LimitedCompanyMatch(None, None), UnincorporatedMatch(Some("ACME"), Some(1111111112)), OrdinaryBusinessPartnershipMatch(None, None), LimitedLiabilityPartnershipMatch(None, None))
         implicit val hc = new HeaderCarrier(sessionId = Some(SessionId(s"session-${UUID.randomUUID}")))
-        val result = BusinessCustomerConnector.lookup(businessDetails)
+        when(mockWSHttp.POST[BusinessDetails, HttpResponse](Matchers.any(), Matchers.any(), Matchers.any())(Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Future.successful(HttpResponse(200, Some(matchFailureResponse))))
+        val result = TestBusinessCustomerConnector.lookup(businessDetails)
         await(result).as[JsValue] must be(matchFailureResponse)
       }
 
