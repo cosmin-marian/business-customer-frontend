844f109a4e17eba24900de285669a0b06f2767ea
Avinash Anand <avinash.b.anand@accenture.com>
2015-04-28 16:37:51 +0100

    removed logger for testing

--- app/controllers/BusinessVerificationController.scala
+++ app/controllers/BusinessVerificationController.scala
@@ -5,7 +5,6 @@ import java.util.UUID
 import connectors.{BusinessCustomerConnector, DataCacheConnector}
 import forms.BusinessVerificationForms._
 import models.ReviewDetails
-import play.api.Logger
 import play.api.mvc._
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.http.SessionKeys
@@ -27,7 +26,6 @@ trait BusinessVerificationController extends FrontendController {
    }
 
   def submit(service: String) = Action.async {  implicit request =>
-    Logger.info("*****************session***************"+request.session.get(SessionKeys.sessionId))
     businessDetailsForm.bindFromRequest.fold(
       formWithErrors => Future.successful(BadRequest(views.html.business_verification(formWithErrors, service))),
       value => {
