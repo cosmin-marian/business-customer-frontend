c6bd6a8ee511d45f1c6bce605f96a52d8b34d210
edwardwheldon <edward.wheldon@digital.hmrc.gov.uk>
2015-05-01 14:02:31 +0100

    Added few  unit tests

--- app/controllers/BusinessRegController.scala
+++ app/controllers/BusinessRegController.scala
@@ -2,7 +2,10 @@ package controllers
 
 
 import play.api.mvc._
-import uk.gov.hmrc.play.frontend.controller.FrontendController
+import uk.gov.hmrc.play.frontend.controller.{UnauthorisedAction, FrontendController}
+import forms.BusinessVerificationForms._
+import uk.gov.hmrc.play.http.SessionKeys
+import java.util.UUID
 
 
 object BusinessRegController extends BusinessRegController{
@@ -11,7 +14,13 @@ object BusinessRegController extends BusinessRegController{
 
 trait BusinessRegController extends FrontendController {
 
-  def register = Action {
-    Ok("Yess")
+  def register(response: String) = Action {
+    Ok("Yes")
   }
+
+  /*
+
+  def businessRegistration = Action { implicit request =>
+      Ok(views.html.business_registration)
+    }*/
 }
---
+++ test/controllers/BusinessRegistrationControllerSpec.scala
@@ -0,0 +1,49 @@
+package controllers
+
+import connectors.{BusinessMatchingConnector, DataCacheConnector}
+import org.jsoup.Jsoup
+import org.mockito.Matchers
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.cache.client.CacheMap
+import uk.gov.hmrc.play.audit.http.HeaderCarrier
+
+import scala.concurrent.Future
+
+class BusinessRegistrationControllerSpec extends PlaySpec with OneServerPerSuite{
+
+  val request = FakeRequest()
+  val service = "ATED"
+
+  object TestBusinessRegController extends BusinessRegController {
+
+
+
+    }
+
+/*  "BusinessRegController" must {
+
+    "respond to /" in {
+      val result = route(FakeRequest(GET, "/business-customer")).get
+      status(result) must not be (NOT_FOUND)
+    }
+
+
+   "return business registration view" in {
+
+     val result = TestBusinessRegController.businessRegistration().apply(FakeRequest())
+     val document = Jsoup.parse(contentAsString(result))
+
+      document.title() must be("Business Registration")
+      document.getElementById("business-registration-header").text() must be("Add business details")
+
+
+  }
+
+
+  }*/
+}
