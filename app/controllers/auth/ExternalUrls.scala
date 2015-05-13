faf2ef932225397e3e9159d63183729b7778d2cf
Avinash Anand <avinash.b.anand@accenture.com>
2015-05-13 16:47:56 +0100

    changed callback URL

--- app/controllers/auth/ExternalUrls.scala
+++ app/controllers/auth/ExternalUrls.scala
@@ -6,7 +6,7 @@ import uk.gov.hmrc.play.config.RunMode
 
 object ExternalUrls extends RunMode {
   val companyAuthHost = s"${Play.configuration.getString(s"govuk-tax.$env.services.auth.company-auth.host").getOrElse("")}"
-  val loginCallback = Play.configuration.getString(s"govuk-tax.$env.services.auth.login-callback.url").getOrElse("/business-customer/business-verification")
+  val loginCallback = Play.configuration.getString(s"govuk-tax.$env.services.auth.login-callback.url").getOrElse("/business-customer")
   val loginPath = s"${Play.configuration.getString(s"govuk-tax.$env.services.auth.login_path").getOrElse("sign-in")}"
   val signIn = s"$companyAuthHost/account/$loginPath?continue=$loginCallback"
 }
