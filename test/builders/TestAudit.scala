71aa6a48c710e9a772e53dbe7623842e5d61039e
Paul Carrielies <paul.carrielies@accenture.com>
2015-07-08 09:48:08 +0100

    ATED-864 Add Auditing to Register Business and Agent

---
+++ test/builders/TestAudit.scala
@@ -0,0 +1,31 @@
+package builders
+
+import java.util.concurrent.ConcurrentLinkedQueue
+
+import uk.gov.hmrc.play.audit.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.model.DataEvent
+import uk.gov.hmrc.play.audit.model.{AuditAsMagnet, Audit, EventTypes}
+import config.BusinessCustomerFrontendAuditConnector
+import uk.gov.hmrc.play.audit.model.Audit._
+
+class TestAudit() extends Audit("test", BusinessCustomerFrontendAuditConnector) {
+  var capturedTxName: String = ""
+  var capturedInputs: Map[String, String] = Map.empty
+  private val dataEvents = new ConcurrentLinkedQueue[DataEvent]
+
+  override def as[A](auditMagnet: AuditAsMagnet[A])(body: Body[A])(implicit hc: HeaderCarrier): A = {
+    this.capturedTxName = auditMagnet.txName
+    this.capturedInputs = auditMagnet.inputs
+    super.as(auditMagnet)(body)
+  }
+
+  def capturedDataEvents: Seq[DataEvent] = dataEvents.toArray(new Array[DataEvent](0)).toSeq
+
+  def captureDataEvent(event: DataEvent) = {
+    this.dataEvents.add(event)
+    ()
+  }
+
+  override def sendDataEvent: (DataEvent) => Unit = captureDataEvent
+}
+
