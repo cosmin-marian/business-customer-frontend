d182f0736a329cd2d65ae2cd2b2eef299d8d2deb
Avinash Anand <avinash.b.anand@accenture.com>
2015-07-28 17:49:16 +0100

    ATED-1071 : added more functionalities to autocomplete;still needs refactoring;

--- public/javascript/ated.js
+++ public/javascript/ated.js
@@ -1,7 +1,12 @@
 function countryCodeAutoComplete() {
-    var combo = $("<input></input>").attr("id", "abc-id").attr("name", "abc").attr("type", "text");
+    var input = $("<input></input>")
+    .attr("id", "businessAddress.country")
+    .attr("name", "businessAddress.country")
+    .attr("class", "form-control")
+    .attr("type", "text");
     var isoListMap = [];
     var select = $('select');
+    var selected = "";
     $(select).each(function(){
         $(this).find('option').each(function(){
             var item = $(this);
@@ -9,6 +14,9 @@ function countryCodeAutoComplete() {
             var value = "";
             if(item.attr('value')) {
                 value = item.attr('value')
+                if(item.attr('selected') == "selected") {
+                    selected = item.attr('value')
+                }
             }
             isoListMap.push({
                 'label': label,
@@ -16,7 +24,17 @@ function countryCodeAutoComplete() {
             });
         });
     });
-
-    $("#SELECTOR").append(combo);
-    $("#abc-id").autocomplete({ source: isoListMap, minLength: 1 });
+//    $("select").remove();
+    input.attr('value')
+    var isErrorThere = $("#businessAddress-country_div").has("div.form-field--error").length
+    console.log(isErrorThere)
+    if(isErrorThere != 0) {
+        $("#businessAddress-country_div").find("div.form-field--error").append(input)
+    }else{
+        $("#businessAddress-country_div").append(input);
+    }
+    $("#businessAddress\\.country").autocomplete({
+        source: isoListMap,
+        minLength: 2
+    });
 }
