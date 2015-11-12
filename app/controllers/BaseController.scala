ae2254ec50e49d180620c60e57e8f416da8d5433
Paul Carrielies <paul.carrielies@accenture.com>
2015-11-12 14:31:22 +0000

    ATED-1424 Add Condifence level

--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -2,11 +2,8 @@ package controllers
 
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.frontend.auth._
-import uk.gov.hmrc.play.frontend.auth.connectors.domain.ConfidenceLevel
 
 trait BaseController extends FrontendController with Actions {
-
-  val GGConfidence = new NonNegotiableIdentityConfidencePredicate(ConfidenceLevel.L50)
 
   def AuthorisedForGG(taxRegime: TaxRegime) = {
     AuthorisedFor(taxRegime = taxRegime, pageVisibility = GGConfidence)
