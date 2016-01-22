450852e972f1e1cdf23d579d47a5c6d0d484c8aa
Avinash Anand <avinash.b.anand@accenture.com>
2016-01-22 15:12:52 +0000

    ATED-1603: fixing play-metrics

--- app/config/ApplicationGlobal.scala
+++ app/config/ApplicationGlobal.scala
@@ -32,7 +32,7 @@ object ApplicationGlobal extends DefaultFrontendGlobal with RunMode {
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): Html =
     views.html.global_error(pageTitle, heading, message)
 
-  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"$env.metrics")
+  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"govuk-tax.$env.metrics")
 
 }
 
