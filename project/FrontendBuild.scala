11cb56f4d08bf9bfcd132a132d9cb32d3251e029
Avinash Anand <avinash.b.anand@accenture.com>
2016-03-11 16:46:48 +0000

    ATED - upgraded play-ui from 4.7.0 -> 4.10.0;play-auth-fe from 4.5.0 -> 4.6.0

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -17,9 +17,9 @@ private object AppDependencies {
 
   private val frontendBootstrapVersion = "5.1.1"
   private val govukTemplateVersion = "4.0.0"
-  private val playUiVersion = "4.7.0"
+  private val playUiVersion = "4.10.0"
   private val httpCachingClientVersion = "5.3.0"
-  private val playAuthorisedFrontendVersion = "4.5.0"
+  private val playAuthorisedFrontendVersion = "4.6.0"
   private val playPartialsVersion = "4.2.0"
   private val playJsonLoggerVersion = "2.1.1"
   private val playConfigVersion = "2.0.1"
