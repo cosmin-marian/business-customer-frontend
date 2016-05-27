cf3ee62cfe5f7cf47d32b06d8747ec0eb2758567
avinash-anand <avinash.anand@digital.hmrc.gov.uk>
2016-05-27 09:27:51 +0100

    ATED-1963 : review comments

--- app/controllers/auth/BusinessCustomerGovernmentGateway.scala
+++ app/controllers/auth/BusinessCustomerGovernmentGateway.scala
@@ -3,8 +3,6 @@ package controllers.auth
 import uk.gov.hmrc.play.frontend.auth.GovernmentGateway
 
 case class BusinessCustomerGovernmentGateway(serviceName: String) extends GovernmentGateway {
-
-//  override val login = ExternalUrls.signIn(serviceName)
   override def loginURL = ExternalUrls.loginURL
   override def continueURL = ExternalUrls.continueURL(serviceName)
 }
