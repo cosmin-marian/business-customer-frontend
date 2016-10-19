77136485547b9d8e20e2655f3d99b6d5c6375041
avinash-anand <avinash.anand@digital.hmrc.gov.uk>
2016-10-19 19:35:43 +0100

    ATED-2508 : added tests for non-uk client registration by an agent

---
+++ test/controllers/nonUKReg/AgentRegisterNonUKClientControllerSpec.scala
@@ -0,0 +1,284 @@
+package controllers.nonUKReg
+
+import java.util.UUID
+
+import models.{Address, ReviewDetails}
+import org.jsoup.Jsoup
+import org.mockito.Matchers
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{AnyContentAsJson, Result}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.BusinessRegistrationService
+import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
+import uk.gov.hmrc.play.http.{HeaderCarrier, SessionKeys}
+
+import scala.concurrent.Future
+
+
+class AgentRegisterNonUKClientControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar with BeforeAndAfterEach {
+
+  val request = FakeRequest()
+  val service = "ATED"
+  val mockAuthConnector = mock[AuthConnector]
+  val mockBusinessRegistrationService = mock[BusinessRegistrationService]
+
+  object TestAgentRegisterNonUKClientController extends AgentRegisterNonUKClientController {
+    override val authConnector = mockAuthConnector
+    override val businessRegistrationService = mockBusinessRegistrationService
+  }
+
+  override def beforeEach(): Unit = {
+    reset(mockAuthConnector)
+    reset(mockBusinessRegistrationService)
+  }
+
+  val serviceName: String = "ATED"
+
+  "BusinessRegController" must {
+
+    "respond to /register" in {
+      val result = route(FakeRequest(GET, s"/business-customer/register/$serviceName/NUK")).get
+      status(result) must not be NOT_FOUND
+    }
+
+    "unauthorised users" must {
+      "respond with a redirect for /register & be redirected to the unauthorised page" in {
+        registerWithUnAuthorisedUser("NUK") { result =>
+          status(result) must be(SEE_OTHER)
+          redirectLocation(result) must be(Some("/business-customer/unauthorised"))
+        }
+      }
+
+      "respond with a redirect for /send & be redirected to the unauthorised page" in {
+        submitWithUnAuthorisedUser("NUK") { result =>
+          status(result) must be(SEE_OTHER)
+          redirectLocation(result) must be(Some("/business-customer/unauthorised"))
+        }
+      }
+    }
+
+    "Authorised Users" must {
+
+      "return business registration view for a Non-UK based client by agent" in {
+
+        registerWithAuthorisedUser(serviceName, "NUK") { result =>
+          status(result) must be(OK)
+          val document = Jsoup.parse(contentAsString(result))
+
+          document.title() must be("Enter your client's overseas business details")
+          document.getElementById("business-verification-text").text() must be("Add a client")
+          document.getElementById("non-uk-reg-header").text() must be("Enter your client's overseas business details")
+          document.getElementById("businessName_field").text() must be("Business name")
+          document.getElementById("businessAddress.line_1_field").text() must be("Address")
+          document.getElementById("businessAddress.line_2_field").text() must be("Address line 2")
+          document.getElementById("businessAddress.line_3_field").text() must be("Address line 3 (optional)")
+          document.getElementById("businessAddress.line_4_field").text() must be("Address line 4 (optional)")
+          document.getElementById("businessAddress.country_field").text() must include("Country")
+          document.getElementById("hasBusinessUniqueId").text() must include("Do you have an overseas Tax Reference?")
+          document.getElementById("submit").text() must be("Continue")
+        }
+      }
+
+    }
+
+    "send" must {
+
+      "validate form" must {
+
+        def createJson(businessName: String = "ACME",
+                       line1: String = "line-1",
+                       line2: String = "line-2",
+                       line3: String = "",
+                       line4: String = "",
+                       country: String = "FR",
+                       hasBusinessUniqueId: Boolean = true,
+                       bUId: String = "some-id",
+                       issuingInstitution: String = "some-institution",
+                       issuingCountry: String = "FR") =
+          Json.parse(
+            s"""
+               |{
+               |  "businessName": "$businessName",
+               |  "businessAddress": {
+               |    "line_1": "$line1",
+               |    "line_2": "$line2",
+               |    "line_3": "$line3",
+               |    "line_4": "$line4",
+               |    "country": "$country"
+               |  },
+               |  "hasBusinessUniqueId": $hasBusinessUniqueId,
+               |  "businessUniqueId": "$bUId",
+               |  "issuingInstitution": "$issuingInstitution",
+               |  "issuingCountry": "$issuingCountry"
+               |}
+          """.stripMargin)
+
+        type InputJson = JsValue
+        type TestMessage = String
+        type ErrorMessage = String
+
+        "not be empty" in {
+          implicit val hc: HeaderCarrier = HeaderCarrier()
+          val inputJson = createJson(businessName = "", line1 = "", line2 = "", country = "", bUId = "", issuingInstitution = "", issuingCountry = "")
+
+          submitWithAuthorisedUserSuccess(FakeRequest().withJsonBody(inputJson)) { result =>
+            status(result) must be(BAD_REQUEST)
+            contentAsString(result) must include("You must enter a business name")
+            contentAsString(result) must include("You must enter an address into Address line 1.")
+            contentAsString(result) must include("You must enter an address into Address line 2.")
+            contentAsString(result) mustNot include("Postcode must be entered")
+            contentAsString(result) must include("You must enter a country")
+            contentAsString(result) must include("You must enter a country that issued the business unique identifier.")
+            contentAsString(result) must include("You must enter an institution that issued the business unique identifier.")
+            contentAsString(result) must include("You must enter a Business Unique Identifier.")
+          }
+        }
+
+        // inputJson , test message, error message
+        val formValidationInputDataSet: Seq[(InputJson, TestMessage, ErrorMessage)] = Seq(
+          (createJson(businessName = "a" * 106), "If entered, Business name must be maximum of 105 characters", "The business name cannot be more than 105 characters."),
+          (createJson(line1 = "a" * 36), "If entered, Address line 1 must be maximum of 35 characters", "Address line 1 cannot be more than 35 characters."),
+          (createJson(line2 = "a" * 36), "If entered, Address line 2 must be maximum of 35 characters", "Address line 2 cannot be more than 35 characters."),
+          (createJson(line3 = "a" * 36), "Address line 3 is optional but if entered, must be maximum of 35 characters", "Address line 3 cannot be more than 35 characters."),
+          (createJson(line4 = "a" * 36), "Address line 4 is optional but if entered, must be maximum of 35 characters", "Address line 4 cannot be more than 35 characters."),
+          (createJson(country = "GB"), "show an error if country is selected as GB", "You cannot select United Kingdom when entering an overseas address"),
+          (createJson(bUId = "a" * 61), "businessUniqueId must be maximum of 60 characters", "Business Unique Identifier cannot be more than 60 characters."),
+          (createJson(issuingInstitution = "a" * 41), "issuingInstitution must be maximum of 40 characters", "The institution that issued the Business Unique Identifier cannot be more than 40 characters."),
+          (createJson(issuingCountry = "GB"), "show an error if issuing country is selected as GB", "You cannot select United Kingdom when entering an overseas address")
+        )
+
+        formValidationInputDataSet.foreach { data =>
+          s"${data._2}" in {
+            implicit val hc: HeaderCarrier = HeaderCarrier()
+            submitWithAuthorisedUserSuccess(FakeRequest().withJsonBody(data._1)) { result =>
+              status(result) must be(BAD_REQUEST)
+              contentAsString(result) must include(data._3)
+            }
+          }
+        }
+
+        "If registration details entered are valid, continue button must redirect to service specific redirect url" in {
+          implicit val hc: HeaderCarrier = HeaderCarrier()
+          val inputJson = createJson()
+          submitWithAuthorisedUserSuccess(FakeRequest().withJsonBody(inputJson)) { result =>
+            status(result) must be(SEE_OTHER)
+            redirectLocation(result) must be(Some("http://localhost:9933/ated-subscription/registered-business-address"))
+          }
+        }
+
+        "valid registration details are entered and BUId question is selected as No, continue button must redirect to service specific redirect url" in {
+          implicit val hc: HeaderCarrier = HeaderCarrier()
+          val inputJson = createJson(hasBusinessUniqueId = false, issuingCountry = "", issuingInstitution = "", bUId = "")
+          submitWithAuthorisedUserSuccess(FakeRequest().withJsonBody(inputJson)) { result =>
+            status(result) must be(SEE_OTHER)
+            redirectLocation(result) must be(Some("http://localhost:9933/ated-subscription/registered-business-address"))
+          }
+        }
+
+        "throw exception, if redirect url is not defined" in {
+          implicit val hc: HeaderCarrier = HeaderCarrier()
+          val inputJson = createJson(hasBusinessUniqueId = false, issuingCountry = "", issuingInstitution = "", bUId = "")
+          submitWithAuthorisedUserSuccess(FakeRequest().withJsonBody(inputJson), "undefined") { result =>
+            val thrown = the[RuntimeException] thrownBy await(result)
+            thrown.getMessage must be("Service does not exist for : undefined. This should be in the conf file against govuk-tax.$env.services.{1}.serviceRedirectUrl")
+          }
+        }
+
+      }
+    }
+  }
+
+  def registerWithUnAuthorisedUser(businessType: String = "NUK")(test: Future[Result] => Any) {
+    val sessionId = s"session-${UUID.randomUUID}"
+    val userId = s"user-${UUID.randomUUID}"
+
+    builders.AuthBuilder.mockUnAuthorisedUser(userId, mockAuthConnector)
+    val result = TestAgentRegisterNonUKClientController.view(serviceName).apply(FakeRequest().withSession(
+      SessionKeys.sessionId -> sessionId,
+      SessionKeys.token -> "RANDOMTOKEN",
+      SessionKeys.userId -> userId))
+
+    test(result)
+  }
+
+  def registerWithAuthorisedAgent(service: String, businessType: String)(test: Future[Result] => Any) {
+    val sessionId = s"session-${UUID.randomUUID}"
+    val userId = s"user-${UUID.randomUUID}"
+
+    builders.AuthBuilder.mockAuthorisedAgent(userId, mockAuthConnector)
+
+    val result = TestAgentRegisterNonUKClientController.view(service).apply(FakeRequest().withSession(
+      SessionKeys.sessionId -> sessionId,
+      SessionKeys.token -> "RANDOMTOKEN",
+      SessionKeys.userId -> userId))
+
+    test(result)
+  }
+
+  def registerWithAuthorisedUser(service: String, businessType: String)(test: Future[Result] => Any) {
+    val sessionId = s"session-${UUID.randomUUID}"
+    val userId = s"user-${UUID.randomUUID}"
+
+    builders.AuthBuilder.mockAuthorisedUser(userId, mockAuthConnector)
+
+    val result = TestAgentRegisterNonUKClientController.view(service).apply(FakeRequest().withSession(
+      SessionKeys.sessionId -> sessionId,
+      SessionKeys.token -> "RANDOMTOKEN",
+      SessionKeys.userId -> userId))
+
+    test(result)
+  }
+
+  def submitWithUnAuthorisedUser(businessType: String = "NUK")(test: Future[Result] => Any) {
+    val sessionId = s"session-${UUID.randomUUID}"
+    val userId = s"user-${UUID.randomUUID}"
+
+    builders.AuthBuilder.mockUnAuthorisedUser(userId, mockAuthConnector)
+
+    val result = TestAgentRegisterNonUKClientController.submit(service).apply(FakeRequest().withSession(
+      SessionKeys.sessionId -> sessionId,
+      SessionKeys.token -> "RANDOMTOKEN",
+      SessionKeys.userId -> userId))
+
+    test(result)
+  }
+
+  def submitWithAuthorisedUserSuccess(fakeRequest: FakeRequest[AnyContentAsJson], service: String = service)(test: Future[Result] => Any) {
+    val sessionId = s"session-${UUID.randomUUID}"
+    val userId = s"user-${UUID.randomUUID}"
+
+    builders.AuthBuilder.mockAuthorisedUser(userId, mockAuthConnector)
+
+    val address = Address("23 High Street", "Park View", Some("Gloucester"), Some("Gloucestershire, NE98 1ZZ"), Some("NE98 1ZZ"), "U.K.")
+    val successModel = ReviewDetails("ACME", Some("Unincorporated body"), address, "sap123", "safe123", isAGroup = false, directMatch = false, Some("agent123"))
+
+    when(mockBusinessRegistrationService.registerBusiness(Matchers.any(), Matchers.any(), Matchers.any(), Matchers.any())(Matchers.any(), Matchers.any())).thenReturn(Future.successful(successModel))
+
+    val result = TestAgentRegisterNonUKClientController.submit(service).apply(fakeRequest.withSession(
+      SessionKeys.sessionId -> sessionId,
+      SessionKeys.token -> "RANDOMTOKEN",
+      SessionKeys.userId -> userId))
+
+    test(result)
+  }
+
+  def submitWithAuthorisedUserFailure(fakeRequest: FakeRequest[AnyContentAsJson])(test: Future[Result] => Any) {
+    val sessionId = s"session-${UUID.randomUUID}"
+    val userId = s"user-${UUID.randomUUID}"
+
+    builders.AuthBuilder.mockAuthorisedUser(userId, mockAuthConnector)
+
+    val result = TestAgentRegisterNonUKClientController.submit(service).apply(fakeRequest.withSession(
+      SessionKeys.sessionId -> sessionId,
+      SessionKeys.token -> "RANDOMTOKEN",
+      SessionKeys.userId -> userId))
+
+    test(result)
+  }
+
+}
