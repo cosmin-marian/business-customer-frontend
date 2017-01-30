dbfea6ed79dbeb6f48626c40e563673fa411aea2
danPudwellOpencast <dan.pudwell@opencastsoftware.com>
2017-01-30 13:13:32 +0000

    removed comments

--- app/controllers/AssetsController.scala
+++ app/controllers/AssetsController.scala
@@ -24,10 +24,10 @@ import scala.concurrent.Future
 object AssetsController extends AssetsBuilder(new HttpErrorHandler {
 
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
-    throw new RuntimeException(s"$getClass onServerError", exception)  //FIXME
+    throw new RuntimeException(s"$getClass onServerError", exception)
   }
 
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
-    throw new RuntimeException(s"$getClass onClientError $statusCode : $message")  //FIXME
+    throw new RuntimeException(s"$getClass onClientError $statusCode : $message")
   }
 })
